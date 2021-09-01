db9234e5fa3b1593ba2d173250f6267b1bc59d77
rozar34 <alexandrwa-roza@yandex.ru>
2021-09-01 15:48:12 +0400

    Initial commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	
+   public Joystick joystick;
+   public CharacterController controller;
+   
+   public float speed;
+   public float gravity;
+   
+   Vector3 moveDirection;
+   
+   void Start(){
+	   
+   }
+   
+   void Update(){
+	   
+	   Vector2 direction = joystick.direction;
+	   
+	   if(controller.isGrounded){
+            moveDirection = new Vector3(direction.x, 0);
+			
+			Quaternion targetRotation = moveDirection != Vector3.zero ? Quaternion.LookRotation(moveDirection) : transform.rotation;
+			transform.rotation = targetRotation;
+	
+            moveDirection = moveDirection * speed;
+        }
+
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+   }
+}
